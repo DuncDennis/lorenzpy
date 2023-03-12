4c8d7e58bcc76bd4868b0517bdbbbb67c8c10aef
DuncDennis <90915296+DuncDennis@users.noreply.github.com>
2023-03-12 16:41:38 +0100

    Added simple documentation with mkdocs.

--- src/lorenzpy/__init__.py
+++ src/lorenzpy/__init__.py
@@ -1,4 +1,10 @@
-"""Python package to simulate and Measure chaotic time series."""
+"""Python package to simulate and Measure chaotic time series.
+
+Modules exported by this package:
+
+- `simulations`: Simulate various discrete and continuous chaotic dynamical systems.
+- `measures`: Measures for the chaotic dynamical systems.
+"""
 
 from . import measures, simulations
 
